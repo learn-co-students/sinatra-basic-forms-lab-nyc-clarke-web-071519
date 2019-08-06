e8e23824bd9f1abadd13be9b6e8d19d32e6d088c
Alyssa Farina <alyssafarina695@gmail.com>
2019-08-06 16:58:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -12,7 +12,10 @@ class App < Sinatra::Base
     
   
     post '/puppy' do
-      @puppy = Puppy.new(params[':name'], params[':breed'], params[':months_old'])
+      @puppy = Puppy.create(:name , :breed, :months_old)
+      @name = params[:name]
+      @breed = params [:breed]
+      @months_old = params[:months_old]
       erb :display_puppy
     end
   
--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,19 @@
 class Puppy
 
-   attr_accessor :name , :breed , :months_old
+   attr_accessor :name, :breed, :months_old
+   @@all = Puppy.create(:name, :breed, :months_old)
 
    def initialize(name, breed, months_old)
     @name = name
     @breed = breed
     @months_old = months_old
+    @@all << Puppy
    end
+
+   def self.all
+      Puppy
+   end
+
+
 
 end
